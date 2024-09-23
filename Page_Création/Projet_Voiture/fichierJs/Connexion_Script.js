ced7e88376d1c14eab07a688beb91c291ea05c60
Abdel Jaleel Khrystel Kaltou <abdel-jaleel-khrystel.kaltou.1@ulaval.ca>
2024-09-23 07:51:08 -0400

    Popup

--- Page_Création/Projet_Voiture/fichierJs/Connexion_Script.js
+++ Page_Création/Projet_Voiture/fichierJs/Connexion_Script.js
@@ -1,1 +1,22 @@
+// Récupère les éléments de la page
+const openPopupBtn = document.getElementById('openPopupBtn');
+const closePopupBtn = document.getElementById('closePopupBtn');
+const popup = document.getElementById('popup');
+
+// Ouvre la fenêtre pop-up
+openPopupBtn.addEventListener('click', () => {
+    popup.style.display = 'flex';
+});
+
+// Ferme la fenêtre pop-up
+closePopupBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+
+// Ferme la fenêtre pop-up si on clique en dehors du contenu
+window.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.style.display = 'none';
+    }
+});
 
